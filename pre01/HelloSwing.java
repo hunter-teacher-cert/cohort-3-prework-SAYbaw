26ebf92417e827729461f258d637a969404da7af
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-01-06 23:47:10 +0000

    tested swing components

---
+++ pre01/HelloSwing.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class HelloSwing {
+
+  public static void main (String[] args) {
+
+    JOptionPane.showMessageDialog(null, "Hello from JOptionPane");
+  }
+}
