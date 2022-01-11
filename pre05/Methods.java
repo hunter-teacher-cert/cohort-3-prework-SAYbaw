ec81323085738f59c4c41fdc76aa6cbda761a0d4
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-01-11 16:23:16 +0000

    Did exercise 2

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class Methods {
   public static void main(String[]args){
-    
+    int num1 = 7;
+    int num2 = 3;
+
+    if (isDivisible(num1,num2))
+      System.out.println("Yes");
+      else
+      System.out.println("No");
+  }
+  public static boolean isDivisible(int m, int n){
+    if (m % n == 0 || n % m == 0)
+      return true;
+      else
+      return false;
   }
 }
