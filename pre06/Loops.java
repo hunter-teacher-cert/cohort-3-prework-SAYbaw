e68b550a90339e4b6401f35d5af525a83cdae6ae
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-01-19 13:09:08 +0000

    Completed exercise 2

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -1,5 +1,22 @@
 public class Loops{
   public static void main(String[] args){
+    System.out.println(squareRoot(8.0));
+
+  }
+  public static double squareRoot(double n){
+    final double DIFF = .0001;
+    double guess = n/2;
+    double testDiff = 1;
+    double oldGuess = 0.0;
+
+    do {
+      oldGuess = guess;
+      guess = (guess + n/guess) / 2; 
+      testDiff = Math.abs(guess - oldGuess);
+    }while (testDiff > DIFF);
     
+    return guess;
+  
+
   }
 }
