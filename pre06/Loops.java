0a4377c8579d74f4f879c6b93e5c8daa423dc2f3
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-01-20 05:03:36 +0000

    completed exercise 4

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -2,6 +2,7 @@ public class Loops{
   public static void main(String[] args){
     System.out.println(squareRoot(8.0));
     System.out.println(power(2,0));
+    System.out.println(factorial(18));
   }
   public static double squareRoot(double n){
     final double DIFF = .0001;
@@ -11,7 +12,7 @@ public class Loops{
 
     do {
       oldGuess = guess;
-      guess = (guess + n/guess) / 2; 
+      guess = (guess + n / guess) / 2; 
       testDiff = Math.abs(guess - oldGuess);
     }while (testDiff > DIFF);
 
@@ -24,5 +25,14 @@ public class Loops{
         answer *= x;
       
       return answer;
+    }
+    public static long factorial(int n){
+      long result = n;
+
+      for (int i = n;i > 1;i--){
+        result *= (i - 1);
+      }
+      
+      return result;
     }
 }
